f7795272a08eeeb29ea8256a6c1e991eee7d3a36
Geovanne Washington <geoxp98@gmail.com>
2025-03-31 12:03:42 -0300

    [FIXED]: little bug

---
+++ week2/strlen-return-test/index.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Second version: convert case program */
+void convert_case(char string[]);
+
+int main(int argc, char *agrv[])
+{
+    string rand_str = get_string("inser any text here ");
+    convert_case(rand_str);
+
+    printf("%s\n", rand_str);
+    return 0;
+}
+
+void convert_case(char string[])
+{
+    for (int i = 0, length = strlen(string); i < length; i++) {
+        // check if input is lowercase
+        if (string[i] >= 'a' && string[i] <= 'z') {
+            puts("há caracteres lower");
+            string[i] -= 32;
+            continue;
+        } 
+        
+        // check if input is uppercase
+        if (string[i] >= 'A' && string[i] <= 'Z') {
+            puts("há caracteres upper");
+            string[i] += 32;
+        } 
+    }
+}
+
