3b7d4883cb99760c5d43cfc04ce24b809cf2c753
Geovanne Washington <geoxp98@gmail.com>
2025-04-04 13:25:31 -0300

    First steps Readability PS

---
+++ week2/problem-set02/readability/readability.c
@@ -0,0 +1,65 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+int main(void)
+{
+    // Prompt the user for some text
+    string text = get_string("Text: ");
+
+    // Count the number of letters, words, and sentences in the text
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    // Compute the Coleman-Liau index
+    // index = 0.0588 * L - 0.296 * S - 15.8
+    // int index = round(0.0588 * (letters * (100 / words)) - 0.296 * ((words / sentences) * 100) - 15.8);
+
+    // Print the grade level
+    printf("Num of letters: %i\nNum of words: %i\nNum of sentences: %i\n", letters, words, sentences); 
+}
+
+int count_letters(string text)
+{
+    // Return the number of letters in text
+    int counter = 0;
+    for (int i = 0, length = strlen(text); i < length; i++)
+        if (isalpha(text[i]))
+            counter++;
+    return counter;
+}
+
+int count_words(string text)
+{
+    // Return the number of words in text
+    // any sequence of characters separated by a space to be a word
+    int counter = 0;
+
+    for (int i = 0, length = strlen(text); i < length + 1; i++) 
+        if (text[i] == ' ' || text[i] == '\0')
+            counter++;
+    
+    return counter;
+}
+
+int count_sentences(string text)
+{
+    // Return the number of sentences in text
+    // You can consider any sequence of characters that ends with a . or a ! or a ? to be a sentence.
+    char punctuation[3] = {'.', '!', '?'};
+
+    int counter = 0;
+    for (int i = 0, length = strlen(text); i < length; i++)
+        for (int j = 0; j < 3; j++)
+            if (text[i] == punctuation[j])
+                counter++;
+    
+    return counter == 0 ? 1 : counter;
+}
