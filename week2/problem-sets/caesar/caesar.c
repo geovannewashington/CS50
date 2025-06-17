df05f89f203227340ca059272214313840b7fb2e
Geovanne Washington <geoxp98@gmail.com>
2025-06-17 13:51:29 -0300

    caesar: some more steps

--- week2/problem-sets/caesar/caesar.c
+++ week2/problem-sets/caesar/caesar.c
@@ -4,23 +4,26 @@
 #include <string.h>
 #include <math.h>
 
+//  TODO: change this char array pointer to string array since we're importing cs50.h
 int convert_to_int(char *char_arr);
 
 int main(int argc, char *argv[])
 {
+    // Assumes argv[1], which is the key, is valid by default 
     int is_arg_valid = 1;
-    
-    for (int i = 0; i < argc; i++) {
+
+    // where k is key
+    int k = convert_to_int(argv[1]);
+
+    for (int i = 0, k_len = strlen(argv[1]); i < k_len; i++) 
         if (!isdigit(argv[1][i]))
-            is_arg_valid = 0;
-    }
+            is_arg_valid = 0; // -> if it's not a digit then it's not valid
     
     if (argc == 1 || argc > 2 || is_arg_valid == 0) {
         printf("Usage: ./caeser key\n");
         return 1;
     } 
     
-    int k = convert_to_int(argv[1]);
     printf("%d\n", k);
     return 0;
 }
