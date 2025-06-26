19336167c76a84e2e1b42d658de79db97328719e
Geovanne Washington <geoxp98@gmail.com>
2025-06-26 14:26:45 -0300

    [DONE]: Substitution problem-set solved

--- week2/problem-sets/substitution/substitution.c
+++ week2/problem-sets/substitution/substitution.c
@@ -3,26 +3,38 @@
 #include <string.h>
 #include <ctype.h>
 
+void cipher(char plaintext[], char key[], int key_length);
+
 int main(int argc, char *argv[])
 {
     // accepts a single command-line argument, the key. (26 character string)
     // the key should be case-insensitive (upper or lower shouldn't change behavior of program)
-    
+    if (argc != 2) {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
     // [FLAG]: assumes key valid by default.  
     int is_key_valid = 1;
     
     // if the key is invalid (not containing 26 chars, containing a char that is not an alphabetic)
-    int key_length = strlen(argv[0]);
+    int key_length = strlen(argv[1]);
     
     if (key_length != 26) 
         is_key_valid = 0;
 
-    for (int i = 0; i < key_length; i++)
-        if (!(isalpha(argv[0][i]))) 
+    int j = 0;
+    for (int i = 0; i < key_length; i++) {
+        // set flag as false and skips 
+        if (!(isalpha(argv[1][i]))) {
             is_key_valid = 0;
+            continue;
+        }
+        // verify that key contains each letter of alphabet at least once.
+        j += tolower(argv[1][i]);
+    }
+    if (j != 2847)
+        is_key_valid = 0;
     
-    // TODO: THIS 👉 // not containing each letter once
-      
     // if program is executed without any arguments or with more than just a single one
     // print an error message with printf and return 1 
     if (argc != 2 || !is_key_valid) {
@@ -30,13 +42,40 @@ int main(int argc, char *argv[])
         return 1;
     }
     
-    // printf an error message and return 1
-    
     // print 'plaintext: ' without a newline a prompt the user for a string using get_string
+    string plaintext = get_string("plaintext: ");
     
+    cipher(plaintext, argv[1], key_length);
+
     // output: ciphertext: 
     // non-alphabetical characters should remain unchanged
     // after outputing ciphertext, the program should print a newline.   
     // NOTE: the output must preserve case.
     return 0;
 }
+
+void cipher(char plaintext[], char key[], int key_length) 
+{
+    char cipher[strlen(plaintext)];
+    int i;
+    for (i = 0; i < key_length; i++) {
+        if (isalpha(plaintext[i])) {
+            if (isupper(plaintext[i])) {
+                cipher[i] = toupper(key[plaintext[i] - 'A']);
+            } else {
+                cipher[i] = tolower(key[plaintext[i] - 'a']);
+            }
+        } else {
+            // change nothing.
+            cipher[i] = plaintext[i];
+        }
+    }
+    cipher[i] = '\0';
+    printf("ciphertext: %s", cipher); 
+    putchar('\n');
+}
+
+
+
+
+
