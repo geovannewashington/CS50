47d1f6c7667d7e186cdd9c00765b9bf39bb6fec8
Geovanne Washington <geoxp98@gmail.com>
2025-11-28 17:43:07 -0300

    week4: first working version: volume

--- week4/problem-sets/volume/volume.c
+++ week4/problem-sets/volume/volume.c
@@ -4,45 +4,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// -- Macros
-
 // Number of bytes in .wav header
 #define HEADER_SIZE 44
-#define SAMPLE_SIZE 2
+#define SAMPLE_SIZE 2 // each sample == 2 bytes each
+
+typedef uint8_t BYTE;
+
 int main(int argc, char **argv)
 {
     // Check command-line arguments
-    if (argc != 4)
-    {
+    if (argc != 4) {
         fprintf(stderr, "Usage: ./volume input.wav output.wav factor\n");
         return EXIT_FAILURE;
     }
 
     // Open files and determine scaling factor
-    FILE *input = fopen(argv[1], "r");
-    if (!input) {
+    FILE *input = fopen(argv[1], "rb"); // read
+    if (input == NULL) {
         perror("fopen");
         return EXIT_FAILURE;
     }
 
-    FILE *output = fopen(argv[2], "w"); // if it doesn't exist, it will get created
-    if (!output) {
+    FILE *output = fopen(argv[2], "wb"); // write, if it doesn't exist, it will get created
+    if (output == NULL) {
         perror("fopen");
         return EXIT_FAILURE;
     }
 
     float factor = atof(argv[3]); 
-    size_t byte;
+    
+    // Copy header to 'output.wav', that is, the first 44 bytes of 'input.wav'
+    BYTE buffer[HEADER_SIZE];  
+    
+    fread(buffer, sizeof(BYTE), HEADER_SIZE, input);
+    fwrite(buffer, sizeof(BYTE), HEADER_SIZE, output);
 
-    // TODO: Copy header from input file to output file
-    while (fread(&byte, sizeof(byte), HEADER_SIZE, input) != 0) {
-        fwrite(&byte, sizeof(byte), HEADER_SIZE, output);
-    }
+    // For each sample, multiply by 'factor' and copy to 'output'
+    int16_t sample_buff;
 
-    // TODO: Read samples from input file and write updated data to output file
-    // read each sample (2-byte each), multiply it by the factor, and then write it to output wav
-    while (fread(&byte, sizeof(byte), SAMPLE_SIZE, input) != 0) {
-        fwrite(&byte, sizeof(byte), SAMPLE_SIZE, output);
+    while (fread(&sample_buff, sizeof(BYTE), SAMPLE_SIZE, input) != 0) {
+        sample_buff = sample_buff * factor;
+        fwrite(&sample_buff, sizeof(BYTE), SAMPLE_SIZE, output);
     }
     
     // Close files
