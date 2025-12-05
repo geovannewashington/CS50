7a709d8ddb8597dece5495ff4442a751f5733c18
Geovanne Washington <geoxp98@gmail.com>
2025-12-05 18:40:57 -0300

    filter-less - feat: sepia filter

--- week4/problem-sets/filter-less/helpers.c
+++ week4/problem-sets/filter-less/helpers.c
@@ -26,8 +26,49 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 }
 
 // Convert image to sepia
+typedef struct {
+    int sepiaRed;
+    int sepiaGreen;
+    int sepiaBlue;
+} Sepia_colors;
+
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    // instance of Sepia_colors
+    Sepia_colors new_colors = {};
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            // for each pixel        
+            
+            // calculate each sepia color            
+            new_colors.sepiaRed = round(
+                    0.393 * image[i][j].rgbtRed
+                 +  0.769 * image[i][j].rgbtGreen
+                 +  0.189 * image[i][j].rgbtBlue
+            );
+
+            new_colors.sepiaGreen = round(
+                    0.349 * image[i][j].rgbtRed
+                 +  0.686 * image[i][j].rgbtGreen
+                 +  0.168 * image[i][j].rgbtBlue
+            );
+            
+            new_colors.sepiaBlue = round(
+                    0.272 * image[i][j].rgbtRed
+                 +  0.534 * image[i][j].rgbtGreen
+                 +  0.131 * image[i][j].rgbtBlue
+            );
+            
+            if (new_colors.sepiaRed > 255) new_colors.sepiaRed = 255;
+            if (new_colors.sepiaGreen > 255) new_colors.sepiaGreen = 255;
+            if (new_colors.sepiaBlue > 255) new_colors.sepiaBlue = 255;
+
+            image[i][j].rgbtRed = new_colors.sepiaRed;
+            image[i][j].rgbtGreen = new_colors.sepiaGreen;
+            image[i][j].rgbtBlue = new_colors.sepiaBlue;
+        }
+    }
     return;
 }
 
